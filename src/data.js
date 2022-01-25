809951208d434ccd2bfe9e7a64959d5c213db330
dignacio39 <idarwin1998@gmail.com>
2022-01-26 06:00:54 +0800

    my first commit

---
+++ src/data.js
@@ -0,0 +1,50 @@
+export const projects = [
+    {
+      title: "React Reserve",
+      subtitle: "MERN Stack",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./thislogo.jpg",
+      link: "",
+    },
+    {
+      title: "React Tracks",
+      subtitle: "React and Python",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./thislogo.jpg",
+      link: "",
+    },
+    {
+      title: "DevChat",
+      subtitle: "React and Firebase",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./thislogo.jpg",
+      link: "",
+    },
+    {
+      title: "Epic Todo App",
+      subtitle: "React Hooks",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./thislogo.jpg",
+      link: "",
+    },
+];
+
+export const skills = [
+    {
+        skill: "HTML",
+    },
+    {
+        skill: "CSS",
+    },
+    {
+        skill: "JavaScript",
+    }
+];
+
+export const testimonials = [
+    
+];
